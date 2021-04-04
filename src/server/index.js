22c2e059357d79f2c72f80f9699a371405757253
srikanth <polineni.srikanth@gmail.com>
2021-04-05 02:21:20 +0530

    Added react app

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,5 @@
+const environment = process.env.ENVRONMENT || 'local';
+require('dotenv').config({path: `.env.${environment}`});
 const { createServer } = require('http');
 const express = require('express');
 const ws = require('ws');
@@ -14,11 +16,14 @@ app.get("/health", (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     return res.json({ "status": "UP" });
 });
-app.all('/secret', (req, res, next) => {
-    //TODO: validate Authentication
+app.use('/api/*', (req, res, next) => {
+    //TODO: Authentication check
+    if(false) {
+        res.send(401, 'Unauthorized');
+    }
     next();
 });
-app.use("/api/servers", require("./routes/api/servers"));
+app.use("/api/servers", require("./routes/servers"));
 
 const webServer = createServer(app);
 
@@ -27,7 +32,7 @@ const webSocketServer = new ws.Server({ server: webServer });
 webSocketServer.on("connection", (webSocket) => {
 
     webSocket.id = uniqid('Server-');
-
+    //req.headers['x-forwarded-for'] || req.connection.remoteAddress
     console.log(`[${strDateTimeNow()}] ${webSocket.id} connected from ${webSocket._socket.remoteAddress}:${webSocket._socket.remotePort}`);
 
     app.locals.servers = webSocketServer.clients;
